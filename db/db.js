c3cbf9831655fb7ea87a788e0eec5b04ca2f2552
Abuu254 <abubakarramadhan254@gmail.com>
2023-06-21 08:19:31 +0900

    portfolio

---
+++ db/db.js
@@ -0,0 +1,153 @@
+export const bio = [
+  "Hello there &#128075;",
+  "I'm Ramadhan Abdi, currently pursuing a Computer Science degree at Yale University. Driven by a passion for software development and a profound belief in the transformative power of technology, my mission is to leverage these attributes in creating solutions that positively impact our world.",
+  "Apart from coding, I love playing soccer, mentoring students, and participating in charity events.",
+  "As a lifelong learner, I'm always seeking new knowledge and experiences to broaden my perspective and enhance my skills.",
+  "Thanks for taking the time to learn a little about me!",
+];
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    skillName: "Python, C, C++, JavaScript",
+    color: "1",
+    percentage: "80",
+  },
+  {
+    title: "Web Technologies",
+    skillName: "HTML 5, CSS 3, Flask",
+    color: "6",
+    percentage: "70",
+  },
+  {
+    title: "Database Management",
+    skillName: "SQLite",
+    color: "4",
+    percentage: "70",
+  },
+  {
+    title: "Version Control",
+    skillName: "Git",
+    color: "7",
+    percentage: "70",
+  },
+  {
+    title: "Web Development",
+    skillName: "Full-Stack Web Programming",
+    color: "3",
+    percentage: "75",
+  },
+  {
+    title: "Data Analysis",
+    skillName: "Data Structures, Algorithms, Data Exploration",
+    color: "5",
+    percentage: "70",
+  },
+  {
+    title: "Editor",
+    skillName: "VS Code",
+    color: "6",
+    percentage: "70",
+  },
+];
+
+export const projects = [
+    {
+      projectName: "TalksAtYale",
+      image: "images/talksatyale.png",
+      summary:
+        "Spearheaded a team of four students in the full-stack development of a secure web application, employing cutting-edge technologies such as React.js, Flask, and SQLAlchemy. Masterfully designed and implemented a variety of API routes and optimized database queries.",
+      preview: "https://github.com/Abuu254/talksatyale",
+      techStack: ["Reactjs", "Flask", "SQLAlchemy"],
+    },
+    {
+      projectName: "Commerce Website",
+      image: "images/market.png",
+      summary:
+        "Implemented a dynamic and responsive web application for a marketplace platform using Flask, Python, and SQLAlchemy. Utilized various features such as user authentication, file uploads, and database management to enable users to list and purchase items.",
+      preview: "https://github.com/Abuu254/talksatyale",
+      techStack: ["Flask", "Python", "SQLAlchemy"],
+    },
+  ];
+
+  export const experience = [
+    {
+      title: "Yale University | African American Studies",
+      duration: "June 2023 - Present",
+      subtitle: "Research Assistant",
+      details: [
+        "Leveraging advanced web scraping techniques to facilitate comprehensive literature searches, aiding vital research methodologies aimed at combating racism in healthcare.",
+        "Applying innovative approaches and leveraging technical skills to create datasets that accurately account for race among medicine's gatekeepers, contributing to the advancement of equitable healthcare practices."
+      ],
+      tags: ["Python", "Web Scraping", "Data Analysis"],
+      icon: "heartbeat",
+    },
+    {
+      title: "Yale Library | Digital Reformatting & Microfilm Services",
+      duration: "September 2021 - May 2023",
+      subtitle: "Student Imaging Specialist",
+      details: [
+        "Led the daily digitization of over 200 pages of the library’s rare resources, employing high-quality scanning and specialized software for processing.",
+        "Developed and deployed Python automation scripts to enhance the digitization process, significantly increasing efficiency by identifying and removing duplicate materials and empty folders in the university’s library digital collection."
+      ],
+      tags: ["Python", "Automation", "Digitization"],
+      icon: "archive",
+    },
+    {
+      title: "Cross World Africa",
+      duration: "June 2022 - August 2022",
+      subtitle: "Summer Intern",
+      details: [
+        "Undertook rigorous field visits to over 15 CWA beneficiaries in Kenya's Rift Valley region, collecting valuable information and drafting insightful reports on program impact, thus aiding in refining the initiative further.",
+        "Effectively mentored 37 scholarship students and actively participated in charity events, embodying the organization's mission to combat poverty and foster academic and personal success among the youth."
+      ],
+      tags: ["Field Work", "Mentorship", "Charity"],
+      icon: "globe-africa",
+    },
+  ];
+
+  export const education = [
+    {
+      title: "Yale University",
+      duration: "Expected Graduation: May 2025",
+      subtitle: "Major: Computer Science",
+      details: [
+        "I am continually excited by the opportunities for intellectual growth at Yale, regularly exploring beyond the core curriculum to engage with diverse classes. This quest for knowledge is not confined to the classroom—I am constantly seeking to apply what I've learned in practical scenarios through the initiation and completion of various mini projects."
+      ],
+      tags: ["Data Structures and Programming Techniques", "Algorithms", "Full-Stack Web Programming", "Data Exploration and Analysis"],
+      icon: "university",
+    },
+  ];
+
+
+
+export const footer = [
+  {
+    label: "Dev Profiles",
+    data: [
+      {
+        text: "GitHub",
+        link: "https://github.com/Abuu254",
+      }
+    ],
+  },
+  {
+    label: "Resources",
+    data: [
+      {
+        text: "Enable Dark/Light Mode",
+        func: "enableDarkMode()",
+      },
+    ],
+  },
+  {
+    label: "Social Profiles",
+    data: [
+      {
+        text: "Linkedin",
+        link: "https://ke.linkedin.com/in/abubakar-ramadhan-2112a4201",
+      },
+
+    ],
+  },
+];
